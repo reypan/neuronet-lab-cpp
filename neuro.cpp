bda3287cc0715a3c2a674103ec87f08779da1312
Andrey Pavlov <andrey.n.pavlov@gmail.com>
2026-01-04 13:25:59 +0700

    Add Exit Confirmation

--- neuro.cpp
+++ neuro.cpp
@@ -8,27 +8,27 @@ using namespace std;
 #include <iomanip>
 #include <iostream>
 
-const int N_SL = 3, //число слоев = 3 + нулевой
+const int N_LAYER = 3, //число слоев = 3 + нулевой
     N_MAX = 30, //максимально возможное число нейронов в слое
     N_MIN = 4, //минимально возможное число нейронов в слое
-    N_SH = 10, //число шаблонов
-    KSO = 1,   //Коэффициент скорости обучения
+    N_PAT = 10, //число шаблонов (patterns)
+    LR = 1,   //Коэффициент скорости обучения (leaning rate)
     N_I = 15000; // максимальное количество итераций в цикле обучения
-const float E_P = 0.01; // пороговая ошибка обучения
+const float ERR_MAX = 0.01; // пороговая ошибка обучения
 
-int struc[N_SL + 1] = {N_MAX, 30, 25, N_MIN}; // структура сети
-float w[N_SL + 1][N_MAX][N_MAX];              //веса
+int struc[N_LAYER + 1] = {N_MAX, 30, 25, N_MIN}; // структура сети
+float w[N_LAYER + 1][N_MAX][N_MAX];              //веса
 /*
   w[k][2][3]
   k - номер слоя
   2 - номер нейрона в (k-1) слое
   3 - номер нейрона в k-ом слое}
 */
-float pattern[N_SH][N_MAX]; //совокупность шаблонов
+float pattern[N_PAT][N_MAX]; //совокупность шаблонов
 // pattern[1][2] - второй пиксел шаблона №1
-float target[N_SH][N_MIN]; //целевой вектор
-float outs[N_SL + 1][N_MAX]; //Выходные значения нейронов в каждом слое
-float delta[N_SL + 1][N_MAX]; //сигналы ошибки дельта
+float target[N_PAT][N_MIN]; //целевой вектор
+float outs[N_LAYER + 1][N_MAX]; //Выходные значения нейронов в каждом слое
+float delta[N_LAYER + 1][N_MAX]; //сигналы ошибки дельта
 float eps;                    // Эпсилон
 
 int c, //вспомогательный счетчик
@@ -36,6 +36,8 @@ int c, //вспомогательный счетчик
     k, //номер слоя
     j, //номер нейрона в предыдущем слое
     i; //номер нейрона в текущем слое
+
+bool exitFlag;
 
 
 #define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
@@ -53,13 +55,15 @@ void printProgress(double percentage) {
 // Проход вперед
 void neuroCalc() {
   float net;
-  for (k = 1; k <= N_SL; k++)
+  for (k = 1; k <= N_LAYER; k++) {
     for (i = 0; i < struc[k]; i++) {
       net = 0;
-      for (j = 0; j < struc[k - 1]; j++)
+      for (j = 0; j < struc[k - 1]; j++) {
         net += outs[k - 1][j] * w[k][j][i];
+      }
       outs[k][i] = 1 / (1 + exp(-net));
     }
+  }
 }
 
 
@@ -68,7 +72,7 @@ float calcErr(int m) {
   // Вспомогательная лок. переменная для подсчёта суммы
   float sum = 0;
   for (i = 0; i < N_MIN; i++) {
-    sum += pow(target[m][i] - outs[N_SL][i], 2);
+    sum += pow(target[m][i] - outs[N_LAYER][i], 2);
   }
   return sqrt(sum / N_MIN);
 }
@@ -77,7 +81,7 @@ float calcErr(int m) {
 // Расчет суммарной ошибки по всем шаблонам
 float calcSumErr(void) {
   float sum = 0;
-  for (c = 0; c < N_SH; c++) {
+  for (c = 0; c < N_PAT; c++) {
     for (i = 0; i < N_MAX; i++) {
       outs[0][i] = pattern[c][i];
     }
@@ -86,26 +90,29 @@ float calcSumErr(void) {
 
     sum += pow(calcErr(c), 2);
   }
-  return sqrt(sum / N_SH);
+  return sqrt(sum / N_PAT);
 }
 
 
 // Проход назад
 void goBack(void) {
-  for (k = N_SL; k >= 1; k--)
+  for (k = N_LAYER; k >= 1; k--) {
     for (i = 0; i < struc[k]; i++) {
-      if (k == N_SL)
+      if (k == N_LAYER) {
         eps = target[m][i] - outs[k][i];
+      }
       else {
         eps = 0;
-        for (c = 0; c < struc[k + 1]; c++)
+        for (c = 0; c < struc[k + 1]; c++) {
           eps += delta[k + 1][c] * w[k + 1][i][c];
+        }
       }
       delta[k][i] = outs[k][i] * (1 - outs[k][i]) * eps;
       for (j = 0; j < struc[k - 1]; j++) {
-        w[k][j][i] += KSO * delta[k][i] * outs[k - 1][j];
+        w[k][j][i] += LR * delta[k][i] * outs[k - 1][j];
       }
     }
+  }
 }
 
 
@@ -114,7 +121,7 @@ void educate(void) {
   cout << "Education..." << endl;
 
   int count = 0; // Счётчик итераций
-  m = 0;     // Счётчик шаблонов (глобальный)
+  m = 0;  // Счётчик шаблонов (глобальный)
   float sum = 0;   // Сумма для подсчёта ошибки
   float sigma = 1; // Ошибка по всем шаблонам (ср. квадратическое откл.)
 
@@ -127,8 +134,8 @@ void educate(void) {
     sum += pow(calcErr(m), 2);
     goBack(); // Проход назад
     // Выбираем очередную обучающую пару
-    if (m == N_SH - 1) {
-      sigma = sqrt(sum / N_SH);
+    if (m == N_PAT - 1) {
+      sigma = sqrt(sum / N_PAT);
       m = 0;
       sum = 0;
     } else
@@ -136,10 +143,11 @@ void educate(void) {
     count++;
 
     // Вывод прогресс-бара каждый сотый шаг
-    if (count % 100 == 0)
+    if (count % 100 == 0) {
       printProgress((float)count / N_I);
+    }
 
-  } while (sigma > E_P && count < N_I);
+  } while (sigma > ERR_MAX && count < N_I);
 
   cout << "\n";
   cout << "Education is completed for " << count << " iterations." << endl;
@@ -150,7 +158,7 @@ void educate(void) {
 void init(void) {
   // Начальная инициализация весов
   srand(time(NULL));
-  for (k = 1; k <= N_SL; k++)
+  for (k = 1; k <= N_LAYER; k++)
     for (j = 0; j < struc[k - 1]; j++)
       for (i = 0; i < struc[k]; i++)
         w[k][j][i] = -1 + 2 * (float)rand() / RAND_MAX;
@@ -184,7 +192,7 @@ void printNeuronet(void) {
   //Вывод выходного вектора
   cout << "Output: " << endl;
   for (i = 0; i < N_MIN; i++) {
-    cout << setprecision(2) << outs[N_SL][i] << "  ";
+    cout << setprecision(2) << outs[N_LAYER][i] << "  ";
   }
   cout << "\n\n";
 
@@ -286,11 +294,28 @@ void setStruc(void) {
 }
 
 
+// Обработчик выхода из программы
+void exitProgram(void) {
+  char ch = 'n';
+
+  do {
+    cout << "Exit anyway? (y or n)" << endl;
+    cin >> ch;
+  }
+  while (ch != 'y' && ch != 'n');
+
+  if (ch == 'y') exitFlag = true;
+  if (ch == 'n') exitFlag = false;
+}
+
+
 // Основная функция
 int main(void) {
   init(); //инициализация весов
 
   loadPatterns(); //Загрузка шаблонов
+
+  exitFlag = false; //сброс флага выхода
 
   char ch; //код команды
   // Вывод меню на экран
@@ -323,15 +348,16 @@ int main(void) {
       setStruc();
       break;
     case '6':
-      cout << "The program is closed. Goodbye!" << endl;
+      exitProgram();
       break;
     default:
       cout << "Wrong item! Try again!" << endl;
       break;
     }
-  } while (ch != '6');
+  } while (!exitFlag);
 
-  // system("pause");
+  cout << "The program is closed. Goodbye!" << endl;
 
   return 0;
 }
+
